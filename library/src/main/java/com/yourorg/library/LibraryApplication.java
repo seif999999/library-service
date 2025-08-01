7f1c66f904d432ad728c1d18985c5010638a83bb
seifeldin1 <seif1442004@gmail.com>
2025-08-01 16:36:48 +0300

    finsihed part 3 (Add application-dev.yml , application-prod.yml ,  ConfigController.java and LibraryConfig.java and also modified the application.properties)

--- library/src/main/java/com/yourorg/library/LibraryApplication.java
+++ library/src/main/java/com/yourorg/library/LibraryApplication.java
@@ -2,8 +2,10 @@ package com.yourorg.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class LibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
